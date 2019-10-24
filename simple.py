7d3ec214ed763e75fc4afdc24b111b6e7df0f1fa
Guy McBride <guy_mcbride@keysight.com>
2019-10-24 18:38:47 +0100

    Updated simple to 6.3 of app

--- simple.py
+++ simple.py
@@ -6,7 +6,7 @@ print()
 print ('clr version = {}'.format(str(clr.__version__)))
 
 """Import the Keysight automated test app remote library DLL"""
-sys.path.append(r'C:\ProgramData\Keysight\DigitalTestApps\Remote Toolkit\Version 5.90\Tools')
+sys.path.append(r'C:\ProgramData\Keysight\DigitalTestApps\Remote Toolkit\Version 6.3\Tools')
 clr.AddReference("Keysight.DigitalTestApps.Framework.Remote")
 import Keysight.DigitalTestApps.Framework.Remote as KtRemote
 """Connect to the automated test application running on the scope
